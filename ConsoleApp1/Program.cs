864016b4fc35d2307397a2d03aee2428f1904559
KhurramQamar <khurramqamar356@gmail.com>
2021-10-05 23:29:44 +0500

    First Sum variation

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        
+        static void Sum()
+        {
+            int a = 7;int  b = 6;
+            Console.WriteLine($"the sum of assigned integers {a} and {b} is: {a+b}");
+           
+        }
+        static void Sum(int a)
+        {
+            
+            int sum = a + a;
+            Console.WriteLine($"the sum of itself {a} is: {sum}");
+        }
+        static void  Sum(int a,int b)
+        {
+             Console.WriteLine($"the sum of two integers  is: {a+b}");
+        }
+        static void Sum(float a, float b)
+        {
+            Console.WriteLine($"the sum of two float numbers  is: {a + b}");
+        }
+        static void Sum(double a, double b)
+        {
+            Console.WriteLine($"the sum of two double numbers  is: {a + b}");
+        }
+        static void Sum(string  a, string  b)
+        {
+            Console.WriteLine($"the sum of two string is: {a + b}");
+        }
+
+        static void Main(string[] args)
+
+        {
+            Sum();
+            Sum(2);
+            Sum(2, 2);
+            Sum(2.2, 2.2);
+            Sum("FC", "IT");
+        }
+    }
+}
