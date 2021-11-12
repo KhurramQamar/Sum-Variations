809d7580ce5b99c9222dab1b5d4569049f7f5ed0
KhurramQamar <83878391+KhurramQamar@users.noreply.github.com>
2021-11-12 23:46:29 +0500

    Update Program.cs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,7 +8,18 @@ namespace ConsoleApp1
     class Program
     {
         
-       
+        static void Sum()
+        {
+            int a = 7;int  b = 6;
+            Console.WriteLine($"the sum of assigned integers {a} and {b} is: {a+b}");
+           
+        }
+        static void Sum(int num1)
+        {
+            
+            int sum = num1 + num1;
+            Console.WriteLine($"the sum of itself {num1} is: {sum}");
+        }
         static void  Sum(int a,int b)
         {
              Console.WriteLine($"the sum of two integers  is: {a+b}");
